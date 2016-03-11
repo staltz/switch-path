9a23f5584b3259d5d06819972ddf6083a373d1e9
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-03-11 15:42:25 -0500

    refactor(): tidy up source code a bit
    Splits up the monolithic index file. Places utility functions into util file.
    Use for-loops instead of for..in for performance wins.

--- src/index.js
+++ src/index.js
@@ -1,177 +1,109 @@
-function isPattern(candidate) {
-  return typeof candidate === `string` &&
-    (candidate.charAt(0) === `/` || candidate === `*`)
-}
+import {
+  isPattern,
+  isRouteDefinition,
+  traverseRoutes,
+  isNotNull,
+  splitPath,
+  isParam,
+  extractPartial,
+  handleTrailingSlash,
+  unprefixed,
+} from './util'
 
-function isRouteConfigurationObject(routes) {
-  if (typeof routes !== `object`) {
-    return false
+function switchPathInputGuard(path, routes) {
+  if (!isPattern(path)) {
+    throw new Error(`First parameter to switchPath must be a route path.`)
   }
-  for (let path in routes) {
-    if (routes.hasOwnProperty(path)) {
-      return isPattern(path)
-    }
+  if (!isRouteDefinition(routes)) {
+    throw new Error(`Second parameter to switchPath must be an object ` +
+      `containing route patterns.`)
   }
-}
-
-function unprefixed(fullStr, prefix) {
-  return fullStr.split(prefix)[1]
-}
-
-function matchesWithParams(sourcePath, pattern) {
-  const sourceParts = sourcePath.split(`/`).filter(s => s.length > 0)
-  const patternParts = pattern.split(`/`).filter(s => s.length > 0)
-  const params = patternParts.map((patternPart, index) => {
-    if (patternPart.match(/:\w+/) !== null) {
-      return sourceParts[index]
-    } else {
-      return null
-    }
-  }).filter(x => x !== null)
-  const matched = patternParts.every((part, i) =>
-    part.match(/:\w+/) !== null || part === sourceParts[i]
-  )
-  return matched ? params : []
-}
-
-function validateSwitchPathPreconditions(sourcePath, routes) {
-  if (typeof sourcePath !== `string`) {
-    throw new Error(`Invalid source path. We expected to see a string given ` +
-      `as the sourcePath (first argument) to switchPath.`)
-  }
-  if (!isRouteConfigurationObject(routes)) {
-    throw new Error(`Invalid routes object. We expected to see a routes ` +
-      `configuration object where keys are strings that look like '/foo'. ` +
-      `These keys must start with a slash '/'.`)
-  }
-}
-
-function validatePatternPreconditions(pattern) {
-  if (!isPattern(pattern)) {
-    throw new Error(`Paths in route configuration must be strings that start ` +
-      `with a slash '/'.`)
-  }
-}
-
-function isNormalPattern(routes, pattern) {
-  if (pattern === `*` || !routes.hasOwnProperty(pattern)) {
-    return false
-  }
-  return true
-}
-
-function handleTrailingSlash(paramsFn) {
-  if (isRouteConfigurationObject(paramsFn)) {
-    return paramsFn[`/`]
-  }
-  return paramsFn
-}
-
-function getParamsFnValue(paramFn, params) {
-  const _paramFn = handleTrailingSlash(paramFn)
-  if (typeof _paramFn !== `function`) {
-    return _paramFn
-  }
-  return _paramFn.apply(null, params)
-}
-
-function splitPath(path) {
-  const pathParts = path.split(`/`)
-  if (pathParts[pathParts.length - 1] === ``) {
-    pathParts.pop()
-  }
-  return pathParts
-}
-
-function matchPartial(sourcePath, pattern) {
-  const patternParts = splitPath(pattern)
-  const sourceParts = splitPath(sourcePath)
-
-  const matchedParts = []
-
-  for (let i = 0; i < patternParts.length; ++i) {
-    matchedParts.push(sourceParts[i])
-  }
-
-  return matchedParts.join(`/`)
-}
-
-function validatePath(sourcePath, matchedPath) {
-  if (matchedPath === null) {
-    return ``
-  }
-  const sourceParts = splitPath(sourcePath)
-  const matchedParts = splitPath(matchedPath)
-  const validPath = sourceParts.map((part, index) => {
-    if (part !== matchedParts[index]) {
-      return null
-    }
-    return part
-  }).filter(x => x !== null).join(`/`)
-  return validPath
-}
-
-function validate({sourcePath, matchedPath, value, routes}) {
-  let validPath = validatePath(sourcePath, matchedPath)
-  if (!validPath) {
-    validPath = !routes[`*`] ? null : sourcePath
-    const validValue = !validPath ? null : routes[`*`]
-    return {
-      validPath,
-      validValue,
-    }
-  }
-  return {validPath, validValue: value}
 }
 
 function betterMatch(candidate, reference) {
-  if (candidate === null) {
+  if (!isNotNull(candidate)) {
     return false
   }
-  if (reference === null) {
+  if (!isNotNull(reference)) {
     return true
   }
   return candidate.length >= reference.length
 }
 
-function switchPath(sourcePath, routes) {
-  validateSwitchPathPreconditions(sourcePath, routes)
-  let matchedPath = null
-  let value = null
-  for (let pattern in routes) {
-    if (!isNormalPattern(routes, pattern)) {
-      continue
-    }
-    validatePatternPreconditions(pattern)
-    if (sourcePath.search(pattern) === 0 && betterMatch(pattern, matchedPath)) {
-      matchedPath = pattern
-      value = routes[pattern]
-    }
-    const params = matchesWithParams(sourcePath, pattern)
-    if (params.length > 0 && betterMatch(sourcePath, matchedPath)) {
-      matchedPath = matchPartial(sourcePath, pattern)
-      value = getParamsFnValue(routes[pattern], params)
-    }
-    if (isRouteConfigurationObject(routes[pattern]) && params.length === 0) {
-      const child = switchPath(unprefixed(sourcePath, pattern), routes[pattern])
-      const nestedPath = pattern + child.path
-      if (child.path !== null && betterMatch(nestedPath, matchedPath)) {
-        matchedPath = nestedPath
-        value = child.value
-      }
-    }
-    if (pattern === sourcePath) {
-      return {path: pattern, value: handleTrailingSlash(routes[pattern])}
+function matchesWithParams(sourcePath, pattern) {
+  const sourceParts = splitPath(sourcePath)
+  const patternParts = splitPath(pattern)
+
+  const params = patternParts
+    .map((part, i) => isParam(part) ? sourceParts[i] : null)
+    .filter(isNotNull)
+
+  const matched = patternParts
+    .every((part, i) => isParam(part) || part === sourceParts[i])
+
+  return matched ? params : []
+}
+
+function getParamFnValue(paramFn, params) {
+  const _paramFn = handleTrailingSlash(paramFn)
+  return typeof _paramFn === `function` ? _paramFn(...params) : _paramFn
+}
+
+function validatePath(sourcePath, matchedPath) {
+  const sourceParts = splitPath(sourcePath)
+  const matchedParts = splitPath(matchedPath)
+
+  for (let i = 0; i < matchedParts.length; ++i) {
+    if (matchedParts[i] !== sourceParts[i]) {
+      return null
     }
   }
 
-  const {validPath, validValue} = validate({
-    sourcePath,
-    matchedPath,
-    value,
-    routes,
-  })
-  return {path: validPath, value: validValue}
+  return `/${extractPartial(sourcePath, matchedPath)}`
 }
 
-module.exports = switchPath
+function validate({sourcePath, matchedPath, matchedValue, routes}) {
+  let path = matchedPath ? validatePath(sourcePath, matchedPath) : null
+  let value = matchedValue
+  if (!path) {
+    path = routes[`*`] ? sourcePath : null
+    value = path ? routes[`*`] : null
+  }
+  return {path, value}
+}
+
+function switchPath(sourcePath, routes) {
+  switchPathInputGuard(sourcePath, routes)
+  let matchedPath = null
+  let matchedValue = null
+
+  traverseRoutes(routes, function matchPattern(pattern) {
+    if (sourcePath.search(pattern) === 0 && betterMatch(pattern, matchedPath)) {
+      matchedPath = pattern
+      matchedValue = routes[pattern]
+    }
+
+    const params = matchesWithParams(sourcePath, pattern)
+
+    if (params.length > 0 && betterMatch(sourcePath, matchedPath)) {
+      matchedPath = extractPartial(sourcePath, pattern)
+      matchedValue = getParamFnValue(routes[pattern], params)
+    }
+
+    if (isRouteDefinition(routes[pattern]) && params.length === 0) {
+      const child = switchPath(
+        unprefixed(sourcePath, pattern) || `/`,
+        routes[pattern]
+      )
+      const nestedPath = pattern + child.path
+      if (!child.path !== null && betterMatch(nestedPath, matchedPath)) {
+        matchedPath = nestedPath
+        matchedValue = child.value
+      }
+    }
+  })
+
+  return validate({sourcePath, matchedPath, matchedValue, routes})
+}
+
+export default switchPath
